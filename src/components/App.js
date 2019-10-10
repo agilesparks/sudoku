09a5e9a21e0065af61f4dcf9c4299d9b1f674d40
Yaki <yaki.koren@gmail.com>
2019-10-10 15:06:58 +0300

    testing jenkins

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ export default function App() {
   let styles = {
     margin: '20px',
     width: '250px',
-    height: '250px',
+    height:  '250px',
     backgroundColor: 'yellow',
     border  : "1px solid black",
   };
