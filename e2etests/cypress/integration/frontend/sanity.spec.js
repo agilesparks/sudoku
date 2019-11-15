a08158fcc3d85436e8e1e2ec151461be332bb515
yakik <yaki.koren@gmail.com>
2019-11-15 16:11:25 +0200

    added project for cypress

---
+++ e2etests/cypress/integration/frontend/sanity.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="Cypress" />
+
+context('Window', () => {
+    beforeEach(() => {
+     // cy.visit(Cypress.env('BASE_URL'))
+    // cy.log(Cypress.env('BASE_URL'))
+     cy.visit(Cypress.env('BASE_URL'))
+    })
+  
+    it('cy.window() - get the global window object', () => {
+      // https://on.cypress.io/window
+      cy.get('[data-testid="3:2"]').type('1')
+      cy.get('[data-testid="3:3"]').type('1')
+     // cy.get('[data-testid="3:3"]').should('have.css', 'background-color')
+      // .and('eq', 'rgb(233, 87, 63)')
+      cy.get('[data-testid="4:4"]').type('2')
+      cy.get('[data-testid="4:5"]').type('3')
+      cy.get('[data-testid="5:4"]').type('4')
+      cy.get('[data-testid="5:5"]').type('5')
+      cy.get('[data-testid="4:3"]')//.contains('6789'))
+      .should('to.have.value', '6789')
+    })
+  
+   
+  })
+
+  
+  
