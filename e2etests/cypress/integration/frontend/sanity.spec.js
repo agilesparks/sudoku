ec149899ea76d4225d835d813530bb352005343f
yakik <yaki.koren@gmail.com>
2019-11-17 12:44:13 +0200

    e2e test change

--- e2etests/cypress/integration/frontend/sanity.spec.js
+++ e2etests/cypress/integration/frontend/sanity.spec.js
@@ -10,15 +10,22 @@ context('Window', () => {
     it('cy.window() - get the global window object', () => {
       // https://on.cypress.io/window
       cy.get('[data-testid="3:2"]').type('1')
+      cy.get('[data-testid="3:3"]').should('have.css', 'background-color')
+      .and('eq', 'rgb(129, 183, 26)')
       cy.get('[data-testid="3:3"]').type('1')
-     // cy.get('[data-testid="3:3"]').should('have.css', 'background-color')
-      // .and('eq', 'rgb(233, 87, 63)')
+      cy.get('[data-testid="3:3"]').should('have.css', 'background-color')
+       .and('eq', 'rgb(233, 87, 63)')
       cy.get('[data-testid="4:4"]').type('2')
       cy.get('[data-testid="4:5"]').type('3')
       cy.get('[data-testid="5:4"]').type('4')
       cy.get('[data-testid="5:5"]').type('5')
-      cy.get('[data-testid="4:3"]')//.contains('6789'))
-      .should('to.have.value', '6789')
+      cy.get('[data-testid="4:3"]').should('to.have.value', '6789')
+      cy.get('[data-testid="3:4"]').type('{backspace}{backspace}{backspace}')
+      cy.get('[data-testid="3:5"]').should('to.have.value', '789')
+      cy.get('[data-testid="3:5"]').type('{backspace}{backspace}')
+      cy.get('[data-testid="4:3"]').should('to.have.value', '89')
+      cy.get('[data-testid="4:3"]').type('{backspace}')
+      cy.get('[data-testid="5:3"]').should('to.have.value', '9')
     })
   
    
